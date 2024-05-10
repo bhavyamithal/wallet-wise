5983a13695c4e56d141cfef2ebf74c543aae7f51
bhavyamithal <bm820@snu.edu.in>
2024-05-10 20:12:38 +0530

    implement header and welcome UI

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,20 @@
+"use client"; 
+
+import React from 'react'
+import CountUp from 'react-countup'
+
+const AnimatedCounter = ({amount}: {amount: number}) => {
+  return (
+    <div>
+        <CountUp 
+            end={amount} 
+            decimal='.' 
+            prefix='₹' 
+            duration={1.75} 
+            decimals={2}
+        />
+    </div>
+  )
+}
+
+export default AnimatedCounter
